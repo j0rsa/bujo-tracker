dfbe133726e833175af168abaedd6c513bb9a53a
red <red.avtovo@gmail.com>
2020-04-11 21:07:33 +0200

    created main coroutine verticle

--- src/main/kotlin/com/j0rsa/bujo/tracker/Main.kt
+++ src/main/kotlin/com/j0rsa/bujo/tracker/Main.kt
@@ -4,20 +4,31 @@ import com.j0rsa.bujo.tracker.verticle.AppVerticle
 import com.j0rsa.bujo.tracker.verticle.SwaggerVerticle
 import io.vertx.core.Vertx
 import io.vertx.kotlin.core.deployVerticleAwait
-import org.slf4j.LoggerFactory
+import io.vertx.kotlin.coroutines.CoroutineVerticle
 
-suspend fun main() {
-    val logger = LoggerFactory.getLogger("Main")
-    try {
-        with(Vertx.vertx()) {
+class Main : CoroutineVerticle() {
+    override suspend fun start() {
+        with(vertx) {
             deployVerticleAwait(AppVerticle::class.qualifiedName!!)
             if (Config.app.swagger.enabled) {
                 deployVerticleAwait(SwaggerVerticle::class.qualifiedName!!)
             }
             logger.info("Application started")
         }
+    }
+
+    companion object : Logging {
+        val logger = logger()
+    }
+}
+
+suspend fun main() {
+    val vertx = Vertx.vertx()
+    try {
+        vertx.deployVerticleAwait(Main::class.qualifiedName!!)
+        println("Application started")
     } catch (exception: Throwable) {
-        logger.error("Could not start application")
+        println("Could not start application")
         exception.printStackTrace()
     }
 }
